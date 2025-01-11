db6fe9e30794902cbceda3f732d90e5087a7e24b
Ahmad <itxti909@gmail.com>
2025-01-11 11:34:51 +0300

    credentials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,8 @@
 import { GithubSignIn } from "@/components/github-sign-in";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { auth } from "@/lib/auth";
+import { auth, signIn } from "@/lib/auth";
+import { executeAction } from "@/lib/executeAction";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -12,7 +13,7 @@ const Page = async () => {
     redirect("/");
   }
 
-  
+
   return (
     <div className="w-full max-w-sm mx-auto space-y-6">
       <h1 className="text-2xl font-bold text-center mb-6">Sign In</h1>
@@ -33,8 +34,14 @@ const Page = async () => {
       {/* Email/Password Sign In */}
       <form
         className="space-y-4"
-        action={async () => {
+        action={async (formData:FormData) => {
           "use server";
+
+          await executeAction({
+            actionFn: async () => {
+              await signIn('credentials', formData)
+            }
+          })
         }}
       >
         <Input
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,27 @@
 // auth.ts
 
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GitHub from "next-auth/providers/github";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub,
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+        // can get more data like phone number and so on
+      },
+      async authorize(credentials) {
+        // check the information
+        const email = "itxti909@gmail.com";
+        const password = "1234";
+        if (credentials.email === email && credentials.password === password) {
+          return { email, password };
+        }
+        throw new Error("Invalid credentials");
+      },
+    }),
+  ],
 });

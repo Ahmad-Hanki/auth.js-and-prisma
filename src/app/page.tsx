168315663d358a1b43c7aff839b6258952c7a5a2
Ahmad <itxti909@gmail.com>
2025-01-11 11:21:20 +0300

    session object and sign in sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignOut } from "@/components/sign-out";
 import { auth } from "@/lib/auth";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
@@ -8,14 +9,16 @@ const Page = async () => {
 
   if (!session) {
     redirect("/sign-in");
-  } 
+  }
 
-  
   return (
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
-        <p className="text-gray-600">Signed in as:</p>
-        <p className="font-medium">TODO</p>
+        <p className="text-gray-600">Signed in as: </p>
+        <p className="font-medium">{session.user?.email}</p>
+        {session.user?.image && (
+          <Image src={session.user?.image} alt="" width={50} height={50} className="mx-auto"/>
+        )}
       </div>
 
       <SignOut />
